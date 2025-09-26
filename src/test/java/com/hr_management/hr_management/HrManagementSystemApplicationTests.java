a4bc6b78aa305874986b14e1cd64d5156ad2e651
thPhonG-oss <ngthanhphong0817@gmail.com>
2025-09-26 17:30:52 +0700

    initialize  project

---
+++ src/test/java/com/hr_management/hr_management/HrManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hr_management.hr_management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
